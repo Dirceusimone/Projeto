4d02d9b7037faae73ab25b9c6d14d4be5be8b386
Dirceu <107999833+Dirceusimone@users.noreply.github.com>
2023-11-01 09:25:31 -0300

    ss

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const aplausos = document.querySelector("#som_tecla_aplausos");
+
+let controle="tocar"
+
+if(controle=="tocar"){
+    alert("audio pronto para tocar")
+}else {
+    alert("o audio nao esta pronto")
+}
+
 function tocaSom(som){
     alert(som)
 }
 
 tocaSom("clap, clap, clap")
+
+
 
