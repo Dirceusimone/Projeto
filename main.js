121d5780b56df71dd183e58d53e029d8ebd33328
Dirceu <107999833+Dirceusimone@users.noreply.github.com>
2023-11-08 09:06:46 -0300

    sss

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
 for(let contador = 0; contador < listaDeTeclas.length; contador++){  
     const Tecla = listaDeTeclas{contador};
     const idAudio = "#som_"+efeito;
