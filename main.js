2586eb89323c7cc83030840d1c9deebeca50c5e2
Dirceu <107999833+Dirceusimone@users.noreply.github.com>
2023-11-29 09:03:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,13 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     Tecla.onclick = function (){
         tocaSom(idAudio);
     }
+    tecla.onkeydown = function(evento){
+        if(evento.code !='Tab'){
+        tecla.classList.add('ativa');
+    }    
+}
+tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+}
     
